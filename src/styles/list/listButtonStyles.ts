a685d4485b65acb95488a610b9ed282746af781d
Andy Klenzman <andyklenz@gmail.com>
2023-11-14 05:25:29 +0100

    styling

--- src/styles/list/listButtonStyles.ts
+++ src/styles/list/listButtonStyles.ts
@@ -2,7 +2,9 @@ import { StyleSheet } from "react-native";
 
 export const listButtonStyles = StyleSheet.create({
     button: {
-      backgroundColor: 'lightgrey'
+      backgroundColor: 'lightgrey',
+      borderRadius: 50,
+      padding: 10
 
     },
     
